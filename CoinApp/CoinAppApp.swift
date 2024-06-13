4b900aba5fddf22b960bf5cd684ef14b43c81710
Ulaş Ardıl KARACA <ulaskkaraca@gmail.com>
2024-06-13 15:58:01 +0300

    Initial Commit

---
+++ CoinApp/CoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAppApp.swift
+//  CoinApp
+//
+//  Created by Ulaş Ardıl KARACA on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
